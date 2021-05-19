d43adf76bca53c7b7c51c405ca187f01ba29572f
Meni Nuriel <nurielmeni@gmail.com>
2021-05-19 20:35:08 +0300

    s

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -44,7 +44,7 @@
       $(row).addClass('active-tr').siblings('tr').removeClass('active-tr');
 
       // 2a.Remove the close button
-      $(jobActiveShow).prev('tr').find('a.btn-table.download.close-apply').hide().siblings('a').show(); 
+      $(jobActiveShow).prev('tr').find('a.btn-table.download.close-details').hide().siblings('a').show(); 
 
       // 3. Show the details section below the row
       $(jobActiveShow).hide().insertAfter(row).fadeIn(600);
